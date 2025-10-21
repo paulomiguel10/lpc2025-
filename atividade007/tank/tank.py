3376b5f5b0974b87002db302f3cf5a713e19c9a9
Paulo Miguel <paulomcastro32@gmail.com>
2025-10-20 23:10:29 -0400

    Codigo 100% em ingles

--- atividade007/tank/tank.py
+++ atividade007/tank/tank.py
@@ -12,7 +12,7 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 ORANGE = (255, 75, 10)
 BLACK = (0, 0, 0)
-AMARELO = (255, 180, 0)
+YELLOW = (255, 180, 0)
 BROWN = (178, 59, 25)
 BLUE = (50, 50, 180)
 GAME_WIDTH, GAME_HEIGHT = 256, 192
@@ -39,14 +39,15 @@ player2_score = 0
 BASE = Path(__file__).resolve().parent  # script folder
 
 sound_shot = pygame.mixer.Sound(str(BASE.parent / "core" / "shotsound.mp3"))
-sound_colision = pygame.mixer.Sound(
+sound_collision = pygame.mixer.Sound(
     str(BASE.parent / "core" / "colision_sound.mp3")
 )
-# game font
+
+# GAME FONT
 font = pygame.font.Font("atividade007\\core\\PressStart2P-Regular.ttf", 70)
 
 
-class player:
+class Player:
 
     # INIT METHOD
     def __init__(self, x, y, key_left, key_right, image_path, key_up=None):
@@ -61,7 +62,7 @@ class player:
         self.rotation_speed = 3
         self.hit_time = None
 
-        # Muda cor do player 1
+        # Change color of player 1
         if key_left == pygame.K_LEFT:  # player 1
             green_image = self.original_image.copy()
             arr = pygame.surfarray.pixels3d(green_image)
@@ -71,27 +72,27 @@ class player:
             del arr
             self.original_image = green_image
 
-        # Muda cor do player 2
+        # Change color of player 2
         if key_left == pygame.K_a:  # player 2
-            orange_image = self.original_image.copy()
-            arr = pygame.surfarray.pixels3d(orange_image)
+            blue_image = self.original_image.copy()
+            arr = pygame.surfarray.pixels3d(blue_image)
             arr[:, :, 0] = 50
             arr[:, :, 1] = 50
             arr[:, :, 2] = 180
             del arr
-            self.original_image = orange_image
+            self.original_image = blue_image
 
     # UPDATE METHOD
     def update(self):
         keys = pygame.key.get_pressed()
-        # Salva posição antiga para checar colisão com paredes
+        # Save old position to check collision with walls
         old_x, old_y = self.rect.x, self.rect.y
         # Rotate
         if keys[self.key_left]:
             self.angle += self.rotation_speed
         if keys[self.key_right]:
             self.angle -= self.rotation_speed
-        # Att image and rect
+        # Update image and rect
         self.image = pygame.transform.rotate(self.original_image, self.angle)
         self.rect = self.image.get_rect(center=self.rect.center)
         # Move when up key is pressed
@@ -101,7 +102,7 @@ class player:
             self.rect.y -= math.sin(rad) * 2
         # Rotation when hit
         if self.hit_time:
-            if pygame.time.get_ticks() - self.hit_time < 500:  # rotate 0.5s
+            if pygame.time.get_ticks() - self.hit_time < 500:  # rotate for 0.5s
                 self.angle += 30
             else:
                 self.hit_time = None
@@ -119,60 +120,54 @@ class player:
         elif self.rect.centery > WINDOW_HEIGHT:
             self.rect.centery = 0
         # Collision with walls
-        for parede in paredes:
-            if self.rect.colliderect(parede):
+        for wall in walls:
+            if self.rect.colliderect(wall):
                 self.rect.x = old_x
                 self.rect.y = old_y
                 break
 
 
-# CREATING PLAYERS
-# player 1
-player1 = player(
+# CREATE PLAYERS
+player1 = Player(
     x=150,
     y=430,
     key_left=pygame.K_LEFT,
     key_right=pygame.K_RIGHT,
-    key_up=pygame.K_UP,  # tecla para andar
-    image_path=("atividade007\\tank\\tanque.png"))
+    key_up=pygame.K_UP,
+    image_path=("atividade007\\tank\\tanque.png")
+)
 
-# player 2
-player2 = player(
+player2 = Player(
     x=874,
     y=430,
     key_left=pygame.K_a,
     key_right=pygame.K_d,
-    key_up=pygame.K_w,  # tecla para andar
-    image_path=("atividade007\\tank\\tanque.png"))
+    key_up=pygame.K_w,
+    image_path=("atividade007\\tank\\tanque.png")
+)
 
 
-# CREATING WALLS
-def criar_paredes_mapa_combat():
-    paredes = []
-    parede1 = pygame.Rect(0, 95, 1024, 32)
-    parede2 = pygame.Rect(0, 736, 1024, 32)
-    parede3 = pygame.Rect(0, 95, 32, 678)
-    parede4 = pygame.Rect(992, 95, 32, 678)
-    parede5 = pygame.Rect(320, 406, 96, 50)
-    parede6 = pygame.Rect(608, 406, 96, 50)
-    parede7 = pygame.Rect(486, 525, 50, 96)
-    parede8 = pygame.Rect(486, 240, 50, 96)
-    parede9 = pygame.Rect(190, 300, 50, 270)
-    parede10 = pygame.Rect(784, 300, 50, 270)
-    paredes.append(parede1)
-    paredes.append(parede2)
-    paredes.append(parede3)
-    paredes.append(parede4)
-    paredes.append(parede5)
-    paredes.append(parede6)
-    paredes.append(parede7)
-    paredes.append(parede8)
-    paredes.append(parede9)
-    paredes.append(parede10)
-    return paredes
+# CREATE WALLS
+def create_combat_map_walls():
+    walls = []
+    wall1 = pygame.Rect(0, 95, 1024, 32)
+    wall2 = pygame.Rect(0, 736, 1024, 32)
+    wall3 = pygame.Rect(0, 95, 32, 678)
+    wall4 = pygame.Rect(992, 95, 32, 678)
+    wall5 = pygame.Rect(320, 406, 96, 50)
+    wall6 = pygame.Rect(608, 406, 96, 50)
+    wall7 = pygame.Rect(486, 525, 50, 96)
+    wall8 = pygame.Rect(486, 240, 50, 96)
+    wall9 = pygame.Rect(190, 300, 50, 270)
+    wall10 = pygame.Rect(784, 300, 50, 270)
+    walls.extend([
+        wall1, wall2, wall3, wall4, wall5,
+        wall6, wall7, wall8, wall9, wall10
+    ])
+    return walls
 
 
-paredes = criar_paredes_mapa_combat()
+walls = create_combat_map_walls()
 
 
 # MAIN LOOP
@@ -190,17 +185,12 @@ while running:
                 current_time = pygame.time.get_ticks()
                 if current_time - last_shot_time_1 >= shot_cooldown:
                     last_shot_time_1 = current_time  # update last shot time
-                    # Convert angle to match ship movement
                     rad = math.radians(player1.angle - 270)
-                    # distance from center to tip
                     tip_offset = player1.original_image.get_height() / 2
-                    # Calculate ship tip coordinates when shooting
                     tip_x = player1.rect.centerx + math.cos(rad) * tip_offset
                     tip_y = player1.rect.centery - math.sin(rad) * tip_offset
-                    # Bullet direction: same vector used for ship movement
                     shot_dx = math.cos(rad) * shot_speed
                     shot_dy = -math.sin(rad) * shot_speed
-                    # Add bullet to list
                     rect_x = int(tip_x - shot_size // 2)
                     rect_y = int(tip_y - shot_size // 2)
                     shots.append({
@@ -215,11 +205,11 @@ while running:
                     })
 
             # Player 2 movement and shooting
-            if event.key == pygame.K_s:  # Player 2 shoots
+            if event.key == pygame.K_s:
                 sound_shot.play()
                 current_time = pygame.time.get_ticks()
                 if current_time - last_shot_time_2 >= shot_cooldown:
-                    last_shot_time_2 = current_time  # update last shot time
+                    last_shot_time_2 = current_time
                     rad = math.radians(player2.angle - 270)
                     tip_offset = player2.original_image.get_height() / 2
                     tip_x = player2.rect.centerx + math.cos(rad) * tip_offset
@@ -251,10 +241,10 @@ while running:
             shot["owner"] != "player1"
             and shot["rect"].colliderect(player1.rect)
         ):
-            sound_colision.play()
+            sound_collision.play()
             player1_health -= 1
             player2_score += 1
-            player1.hit_time = pygame.time.get_ticks()  # activate rotation
+            player1.hit_time = pygame.time.get_ticks()
             shots.remove(shot)
             continue
         # Collision with player 2
@@ -262,16 +252,16 @@ while running:
             shot["owner"] != "player2"
             and shot["rect"].colliderect(player2.rect)
         ):
-            sound_colision.play()
+            sound_collision.play()
             player2_health -= 1
-            player2.hit_time = pygame.time.get_ticks()  # activate rotation
+            player2.hit_time = pygame.time.get_ticks()
             player1_score += 1
             shots.remove(shot)
             continue
         # Collision with walls
-        for parede in paredes:
-            if shot["rect"].colliderect(parede):
-                shots.remove(shot)  # remove the shot
+        for wall in walls:
+            if shot["rect"].colliderect(wall):
+                shots.remove(shot)
                 break
         shot["rect"].x += shot["dx"]
         shot["rect"].y += shot["dy"]
@@ -288,27 +278,32 @@ while running:
         if current_time - shot["born_time"] >= shot_lifetime:
             shots.remove(shot)
 
-    # Draw players
+    # DRAW PLAYERS
     screen.blit(player1.image, player1.rect)
     screen.blit(player2.image, player2.rect)
-    # Player 1 score
+
+    # PLAYER 1 SCORE
     score_text1 = font.render(f"{player1_score}", True, GREEN)
     screen.blit(score_text1, (256, 20))
-    # Player 2 score
+
+    # PLAYER 2 SCORE
     score_text2 = font.render(f"{player2_score}", True, BLUE)
     screen.blit(score_text2, (768, 20))
-    # Player 1 WINS
+
+    # PLAYER 1 WINS
     score_win1 = font.render("Player 1 Wins", True, GREEN)
     if player1_score >= 3:
         screen.fill(BROWN)
         screen.blit(score_win1, (66, 140))
-    # Player 2 WINS
+
+    # PLAYER 2 WINS
     score_win2 = font.render("Player 2 Wins", True, BLUE)
     if player2_score >= 3:
         screen.fill(BROWN)
         screen.blit(score_win2, (66, 140))
-    for parede in paredes:
-        pygame.draw.rect(screen, AMARELO, parede)
+
+    for wall in walls:
+        pygame.draw.rect(screen, YELLOW, wall)
 
     pygame.display.flip()
     clock.tick(60)
