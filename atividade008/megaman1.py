c8be8cb913cc8a600b164186f0e0d9b1cbbfe821
Paulo Miguel <paulomcastro32@gmail.com>
2025-11-10 15:55:39 -0400

    Megaman atualizado

--- atividade008/megaman1.py
+++ atividade008/megaman1.py
@@ -94,169 +94,6 @@ class Megaman(pygame.sprite.Sprite):
 
         if self.direcao == 1:
             self.image = pygame.transform.flip(self.image, True, False)
-    def update(self):
-        if self. animar == True:
-            self.atual = self.atual + 0.2
-            if self.atual >= len(self.sprites):
-                self.atual = 0
-            self.image = self.sprites[int(self.atual)]
-            self.image = pygame.transform.scale(self.image, (499/5, 500/5))
-            #Inverte imagem se estiver indo para esquerda
-            if self.direcao == 1:
-                self.image = pygame.transform.flip(self.image, True, False)
-  
-        if hasattr(self, "velocidade_y"):
-            self.rect.y += self.velocidade_y
-            self.velocidade_y += 1
-
-            if self.rect.y >= 530:
-                self.rect.y = 530
-                self.velocidade_y = 0
-            else:
-                self.sprites = self.sprites_pular
-                self.image = self.sprites[0]
-                self.image = pygame.transform.scale(self.image, (499/5, 500/5))
-                if self.direcao == 1:
-                  self.image = pygame.transform.flip(self.image, True, False)
-
-class Tiro(pygame.sprite.Sprite):
-    def __init__(self, x, y, direcao):
-        pygame.sprite.Sprite.__init__(self)
-        self.sprites_tiro = [pygame.image.load('atividade008/sprites/tiro.png')]
-        self.image = self.sprites_tiro[0]
-        self.image = pygame.transform.scale(self.image, (30, 15))
-        # tamanho do tiro
-
-        # Inverter caso esteja virado para a direita (1)
-        if direcao == 1:
-            self.image = pygame.transform.flip(self.image, True, False)
-
-        # Posição e direção
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
-        self.direcao = direcao
-        self.velocidade = 15 * direcao
-
-    def update(self):
-        self.rect.x += self.velocidade
-        if self.rect.x < 0 or self.rect.x > 850:
-            self.kill()
-
-#Criação do sprite
-todas_as_sprites = pygame.sprite.Group()
-megaman = Megaman()
-tiros = pygame.sprite.Group()
-todas_as_sprites.add(megaman)
-
-relogio = pygame.time.Clock()
-
-import pygame
-import sys
-
-# Inicialização
-pygame.init()
-
-# Configurações da tela
-screen_size = (850, 720)
-screen = pygame.display.set_mode(screen_size)
-pygame.display.set_caption("Sprites")
-
-#Fundo de tela
-background = pygame.image.load('atividade008/sprites/fundo.png')
-background = pygame.transform.scale(background, screen_size)
-
-class Megaman(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.sprites = []
-
-        #Sprites de andar
-        self.sprites_andar = [
-            pygame.image.load('atividade008/sprites/andar_1.png'),
-            pygame.image.load('atividade008/sprites/andar_2.png'),
-            pygame.image.load('atividade008/sprites/andar_3.png')
-        ]
-        #Sprite ficar parado
-        self.sprites_parado = [
-            pygame.image.load('atividade008/sprites/parado_1.png')
-        ]
-
-        #Sprite de pular
-        self.sprites_pular = [
-            (pygame.image.load('atividade008/sprites/pulo.png'))
-                             ]
-        #Sprite de atirar parado
-        self.sprites_atirarparado = [
-          (pygame.image.load('atividade008/sprites/parado_atira.png'))
-        ]
-        #Sprite de atirar correndo 
-        self.sprites_atirarcorrendo = [
-            pygame.image.load('atividade008/sprites/atira_1.png'),
-            pygame.image.load('atividade008/sprites/atira_2.png'),
-            pygame.image.load('atividade008/sprites/atira_3.png')
-        ]
-        #Define a animação inicial
-        self.sprites = self.sprites_andar
-
-        self.atual = 0
-        self.velocidade = 8 #velocidade de movimento|
-        self.direcao = 1 #direita: 1 esquerda: -1
-        self.image = self.sprites[self.atual]
-        self.image = pygame.transform.scale(self.image, (499/5, 500/5))
-
-        self.rect = self.image.get_rect()
-        self.rect.topleft = 200,530
-        self.animar = False
-    
-    def pular(self):
-        if self.rect.y == 530 and (not hasattr(self, "velocidade_y") or self.velocidade_y == 0):
-
-            self.animar = True
-            self.sprites = self.sprites_pular
-            self.image = self.sprites[0]
-            self.image = pygame.transform.scale(self.image, (499/5, 500/5))
-            self.velocidade_y = -17
-
-    def atirarparado(self):
-        self.animar = True
-        self.atual = 0
-        self.sprites = self.sprites_atirarparado
-        self.image = self.sprites[0] 
-        self.image = pygame.transform.scale(self.image, (499/4.6, 500/4.6))
-
-        if self.direcao == 1:
-            self.image = pygame.transform.flip(self.image, True, False)
-    
-    def atirarcorrendo(self):
-        self.animar = True
-        self.sprites = self.sprites_atirarcorrendo
-        self.atual += 0.3
-        
-        if self.atual >= len(self.sprites):
-            self.atual = 0
-
-        #Atualiza a imagem atual
-        self.image = self.sprites[int(self.atual)]
-        self.image = pygame.transform.scale(self.image, (499/5, 500/5))
-
-        #Se estiver indo para direita
-        if self.direcao == 1:
-            self.image = pygame.transform.flip(self.image, True, False)  
-
-    def andar(self, direcao):
-      self.animar = True
-      self.direcao = direcao
-      self.sprites = self.sprites_andar
-  
-    def parar(self):
-        self.animar = False
-        self.atual = 0
-        self.sprites = self.sprites_parado
-        self.image = self.sprites[0]
-        self.image = pygame.transform.scale(self.image, (499/5, 500/5))
-
-        if self.direcao == 1:
-            self.image = pygame.transform.flip(self.image, True, False)
 
     def update(self):
         if self. animar == True:
@@ -305,7 +142,7 @@ class Tiro(pygame.sprite.Sprite):
         self.rect.x += self.velocidade
         if self.rect.x < 0 or self.rect.x > 850:
             self.kill()
-         
+
 #Criação do sprite
 todas_as_sprites = pygame.sprite.Group()
 megaman = Megaman()
@@ -346,7 +183,7 @@ while running:
             megaman.atirarcorrendo()
         else:
             megaman.andar(1)
-            movendo = True
+        movendo = True
 
     # Movimento para a esquerda
     elif keys[pygame.K_LEFT]:
