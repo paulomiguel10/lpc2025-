9ebc01f909ddb730f08efe12033c06c9b5a364dd
Paulo Miguel <paulomcastro32@gmail.com>
2025-12-01 15:28:41 -0400

    menu atualizado

--- Atividade010/src/sounds.py
+++ Atividade010/src/sounds.py
@@ -17,7 +17,6 @@ sound_shot.set_volume(0.3)
 zombie_sound = pygame.mixer.Sound("atividade010/sounds/zombie_sound.mp3")
 zombie_death = pygame.mixer.Sound("atividade010/sounds/zombie_death.mp3")
 
-background_menu = pygame.mixer_music.load(
-    "atividade010/sounds/background_menu.mp3")
+background_menu = pygame.mixer_music.load("atividade010/sounds/background_menu.mp3")
 background_menu = pygame.mixer.music.set_volume(0.8)
 background_menu = pygame.mixer.music.play(-1)
